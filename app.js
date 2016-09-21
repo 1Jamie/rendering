0525574835c158f890c6f0b267889eed891d88f9
Jamie <charja13@live.com>
2016-09-21 09:33:20 -0400

    removed all external sources as to not violate some origin policy C

--- app.js
+++ app.js
@@ -1,61 +1,15 @@
-///include="/babylon.2.4.js" 
+/// <reference path="babylon.2.4.d.ts" />
+var canvas = document.getElementById("renderCanvas");
+        var engine = new BABYLON.Engine(canvas, true);
+        BABYLON.SceneLoader.Load("", "test.babylon", engine, function (newScene) {
+            // Wait for textures and shaders to be ready
+            newScene.executeWhenReady(function () {
+                // Attach camera to canvas inputs
+                newScene.activeCamera.attachControl(canvas)
 
-window.addEventListener('DOMContentLoaded', function() {
-  var canvas = document.getElementById("renderCanvas");
-  var engine = new BABYLON.Engine(canvas, true);
-
-  var createScene = function(){
-    //create a basic BJS scene object
-    var scene = new BABYLON.Scene(engine);
-    //creating the camera real fast
-    var camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0,5,-10), scene);
-    //target the camera to the scene origin
-    camera.setTarget(BABYLON.Vector3.Zero());
-    //attach the camera to the canvas
-    camera.attachControl(canvas, false);
-    //we need to create some light so that everything is visible
-    var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,-1), scene);
-    //lets create the ground for the camera/view point to sit on
-    var ground = BABYLON.Mesh.CreateGround('ground1', 12,12,1, scene);
-    //create a basic shape for now will run with more later
-    var block = BABYLON.Mesh.CreateBox('box1', 4, scene);
-    block.position.z = 1;
-    block.position.x = 2;
-    block.position.y = -1;
-    //return the scene created
-    return scene;
-    
-};
-
-var scene = createScene();
-
-engine.runRenderLoop(function () {
-    scene.render();
-});
-
-        // Resize
-        window.addEventListener("resize", function () {
-            engine.resize();
+                // Once the scene is loaded, just register a render loop to render it
+                engine.runRenderLoop(function() {
+                    newScene.render();
+                });
+            });
         });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    });
