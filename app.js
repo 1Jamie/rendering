0dce3a393a85deeb6c41608789e2b29c8fd4f48d
Jamie <charja13@live.com>
2016-09-29 14:28:49 -0400

    some major changes and new textures for skybox and grass

--- app.js
+++ app.js
@@ -11,10 +11,6 @@ document.addEventListener("DOMContentLoaded", function() {
         scene.collisionsEnabled = true;
         console.log("scene created");
 
-        //any textures we assign will go here
-        var texture1 = new BABYLON.Texture("grass.jpg", scene);
-        console.log("textures loaded in");
-
         //creating the free camera we are going to use for the viewport
         var camera = new BABYLON.FreeCamera("main", new BABYLON.Vector3(-4, 1, 4), scene);
         console.log("camera generated");
@@ -23,18 +19,46 @@ document.addEventListener("DOMContentLoaded", function() {
         camera.setTarget(BABYLON.Vector3.Zero());
         camera.attachControl(canvas, false);
         console.log("camera attached to canavas");
+        camera.applyGravity = true;
+        camera.speed = 1;
+        //camera.fov, 15;
+
+        //why not make a skybox all nice and stuff
+        var skybox = BABYLON.Mesh.CreateBox("skybox", 100.0, scene);
+        var skyboxMaterial = new BABYLON.StandardMaterial("skybox",scene);
+        skyboxMaterial.backFaceCulling = false;
+        skyboxMaterial.disableLighting=true;
+        skybox.material = skyboxMaterial;
+        skybox.infiniteDistance = true;
+        skyboxMaterial.diffuseColor = new BABYLON.Color3(255,255,255);
+        skyboxMaterial.specularColor = new BABYLON.Color3(255,255,255);
+        //applying specific texturing
+        skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("skybox", scene);
+        skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE
+
+
+        //texture test
+        var materialPlane = new BABYLON.StandardMaterial("texturePlane", scene);
+        materialPlane.diffuseTexture = new BABYLON.Texture("grass2.jpg", scene);
+        //materialPlane.
+        materialPlane.diffuseTexture.uScale = 15.0;//Repeat on the Vertical Axes
+        materialPlane.diffuseTexture.vScale = 15.0;//Repeat on the Horizontal Axes
 
         //we are going to create the ground we are going to stand on
         //and enable the colliders for the ground so we dont fall throught it
-        var ground = new BABYLON.Mesh.CreateGround("grndMain", 10, 10, 2, scene, true);
+        var ground = BABYLON.Mesh.CreatePlane("plane", 120, scene);
+        ground.position.y = -5;
+        ground.rotation.x = Math.PI / 2;
         console.log("ground created");
         ground.checkCollisions = true;
         console.log("ground soldified by colliders");
-        ground.material = texture1;
+        //ground.material = material1;
+        //ground.diffuseTexture = texture1;
+        ground.material = materialPlane;
         console.log("grass sould now be the texutre of the ground");
 
         //create some lighting so we can see
-        var pLight = new BABYLON.PointLight("light1", new BABYLON.Vector3(0,0,10), scene);
+        var pLight = new BABYLON.PointLight("light1", new BABYLON.Vector3(0, 5, 0), scene);
         console.log("light should be created");
 
         return scene;
